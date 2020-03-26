1bf159097838ffdd87cae76e623f93df23f9c999
jkhaui <jordan.lee.163@gmail.com>
2020-03-27 10:53:18 +1100

    Add chevron icons.

---
+++ svg-jsx-icons/ChevronLeftIcon.jsx
@@ -0,0 +1,20 @@
+import React, { forwardRef } from 'react';
+
+export default forwardRef(({ iconSize, stroke, ...rest }, ref) =>
+  <svg
+    width={iconSize}
+    height={iconSize}
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <polyline
+      ref={ref}
+      fill="none"
+      points="17.5 22 6.5 12 17.5 2"
+      stroke={stroke}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="3"
+    />
+  </svg>,
+);
---
+++ svg-jsx-icons/ChevronRightIcon.jsx
@@ -0,0 +1,21 @@
+import React, { forwardRef } from 'react';
+
+export default forwardRef(({ iconSize, stroke, ...rest }, ref) =>
+  <svg
+    height={iconSize}
+    width={iconSize}
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    {...rest}
+  >
+    <polyline
+      ref={ref}
+      fill="none"
+      stroke={stroke}
+      points="6.5 2 17.5 12 6.5 22"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="3"
+    />
+  </svg>,
+);
